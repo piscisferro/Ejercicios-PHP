8c92c4e2cc1bbdd0b085055c1026ad1d1f396eb5
Juan Jose Fernandez Romero <piscis.ferro@gmail.com>
2016-01-04 16:28:54 +0100

    Añadido 1 ejercicio de PHP Orientado a Objetos
    Añadido el primer ejercicio de PHP Orientado a Objetos

---
+++ Capitulo 8 - PHP Orientado a Objetos/Ej1 -/dadoPoker.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Ejercicio 1
+ * Crea la clase DadoPoker. Las caras de un dado de poker tienen las siguientes figuras: As, K, Q, J,
+ * 7 y 8 . Crea el método tira() que no hace otra cosa que tirar el dado, es decir, genera un valor
+ * aleatorio para el objeto al que se le aplica el método. Crea también el método nombreFigura(), que
+ * diga cuál es la figura que ha salido en la última tirada del dado en cuestión. Crea, por último, el
+ * método getTiradasTotales() que debe mostrar el número total de tiradas entre todos los dados.
+ * Realiza una aplicación que permita tirar un cubilete con cinco dados de poker.
+ *
+ * @author Juan Jose Fernandez Romero
+ */
+class DadoPoker {
+    
+    // Guardamos de manera estatica las tiradas totales
+    private $tiradasTotales = 0;
+    
+    // Atributo caras que contendra las caras del dado
+    private static $caras = array("As", "K", "Q", "J", "7", "8");
+    
+    // Atributo donde guardamos la ultima tirada
+    private $ultimaTirada;
+    
+    
+    // Constructor
+    public function __construct() {
+        
+        $this->ultimaTirada = "¡Aun no se ha tirado!";
+        
+    }
+    
+    // Funcion tira 
+    public function tira() {
+        
+        // Generamos un valor aleatorio entre 0 y 5
+        $tirada = mt_rand(0, 5);
+        
+        // Guardamos lo que haya salido en $ultimaTirada
+        $this->ultimaTirada = DadoPoker::$caras[$tirada];
+        
+        // Incrementamos las tiradas totales
+        $this->tiradasTotales++;
+        
+    }
+    
+    // Funcion que devuelve la ultima figura que ha salido en el dado
+    public function nombreFigura() {
+        
+        return $this->ultimaTirada;
+        
+    }
+
+
+    // Funcion getter para recoger el numero de tiradas totales.
+    public function getTiradasTotales() {
+        
+        return $this->tiradasTotales;
+        
+    }
+    
+    
+}
---
+++ Capitulo 8 - PHP Orientado a Objetos/Ej1 -/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+Ejercicio 1
+Crea la clase DadoPoker. Las caras de un dado de poker tienen las siguientes figuras: As, K, Q, J,
+7 y 8 . Crea el método tira() que no hace otra cosa que tirar el dado, es decir, genera un valor
+aleatorio para el objeto al que se le aplica el método. Crea también el método nombreFigura(), que
+diga cuál es la figura que ha salido en la última tirada del dado en cuestión. Crea, por último, el
+método getTiradasTotales() que debe mostrar el número total de tiradas entre todos los dados.
+Realiza una aplicación que permita tirar un cubilete con cinco dados de poker.
+
+Create By Juan Jose Fernandez Romero
+-->
+<?php
+
+// Iniciamos la sesion
+session_start();
+
+// Incluimos la clase dadoPoker.php
+require 'dadoPoker.php';
+
+if (isset($_POST["crea"])) {
+    // Creamos el dado
+    $dado = new DadoPoker();
+    // Lo guardamos en sesion
+    $_SESSION["dado"] = serialize($dado);
+}
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DadoPoker</title>
+    </head>
+    <body>
+        <h1>DadoPoker</h1>
+        <?php
+        
+        // Si no tenemos ningun dado guardado en session
+        if (!isset($_SESSION['dado'])) {
+            
+            // Mostramos el boton para crear dado
+            ?>
+            <form action="index.php" method="post">
+                <input type="submit" name="crea" value="Crear Dado">
+            </form>
+            <?php
+        } else { // Si tenemos el dado ya creado, mostramos el boton para lanzarlo
+            ?>
+            <form action="index.php" method="post">
+                <input type="submit" name="tira" value="Tira el Dado">
+            </form>   
+        <?php
+        }
+        
+        // Si pulsamos tirar el dado
+        if (isset($_POST['tira'])) {
+            
+            // Recuperamos el dado de session
+            $dado = unserialize($_SESSION['dado']);
+            
+            // Tiramos el dado
+            $dado->tira();
+            // Recogemos la figura que haya salido
+            $figura = $dado->nombreFigura();
+            // Recogemos la cantidad de veces que hemos tirado
+            $tiradas = $dado->getTiradasTotales();
+            
+            // Volvemos a guardar el dado en session
+            $_SESSION['dado'] = serialize($dado);
+            
+            // Ahora mostramos en HTML los resultados
+            ?>
+        
+        <p>Figura: <?=$figura?></p>
+        <p>Tiradas Totales: <?=$tiradas?></p>
+        
+            <?php
+        
+        }
+        ?>
+    </body>
+</html>
